61f9835bc92e5fece4cfab54bee3d6e7d65e8997
Ahmed S. <s.ahmed.profiquence@gmail.com>
2026-01-26 15:37:45 +0530

    Have a basic structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${spaceGroteskSans.variable} ${jetbrainsMono.variable} antialiased`}
       >
